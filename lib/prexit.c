d1c2b2c43be0fd5b8eed393ba2d69b73cbaad433
leo <leo07046219@gmail.com>
2016-03-13 09:31:12 +0800

    lib--prexit.c

---
+++ lib/prexit.c
@@ -0,0 +1,25 @@
+/*8-3 打印exit状态的说明*/
+
+#include "apue.h"
+#include <sys/wait.h>
+
+void pr_exit(int status)
+{
+    if (WIFEXITED(status))
+    {
+        printf("normal termination, exit status = %d\n", WEXITSTATUS(status));
+    }
+    else if (WIFSIGNALED(status))
+    {
+		printf("abnormal termination, signal number = %d%s\n", WTERMSIG(status),
+#ifdef	WCOREDUMP
+				WCOREDUMP(status) ? " (core file generated)" : "");
+#else
+				"");
+#endif
+    }
+    else if (WIFSTOPPED(status))
+    {
+        printf("child stopped, signal number = %d\n", WSTOPSIG(status));
+    }
+}
