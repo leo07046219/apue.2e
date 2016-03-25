323ef0bc8fe05e4ebc70ad3af0556a7780f740cc
leo <leo07046219@gmail.com>
2016-03-25 08:14:04 +0800

    10-8--signals--read2.c添加註釋

--- signals/read2.c
+++ signals/read2.c
@@ -6,33 +6,43 @@
 static void		sig_alrm(int);
 static jmp_buf	env_alrm;
 
-int
-main(void)
+int main(void)
 {
-	int		n;
+	int		n = 0;
 	char	line[MAXLINE];
 
-	if (signal(SIGALRM, sig_alrm) == SIG_ERR)
-		err_sys("signal(SIGALRM) error");
-	if (setjmp(env_alrm) != 0)
-		err_quit("read timeout");
+    memset(line, 0, sizeof(line));
+
+    if (signal(SIGALRM, sig_alrm) == SIG_ERR)
+    {
+        err_sys("signal(SIGALRM) error");
+    }
+    if (setjmp(env_alrm) != 0)
+    {
+        err_quit("read timeout");
+    }
 
 	alarm(10);
-	if ((n = read(STDIN_FILENO, line, MAXLINE)) < 0)
-		err_sys("read error");
+
+    if ((n = read(STDIN_FILENO, line, MAXLINE)) < 0)
+    {
+        err_sys("read error");
+    }
+
 	alarm(0);
 
 	write(STDOUT_FILENO, line, n);
+
 	exit(0);
 }
 
-static void
-sig_alrm(int signo)
+static void sig_alrm(int signo)
 {
 	longjmp(env_alrm, 1);
 }
 
 /*
-无需担心一个慢速系统调用是否被中断
-
+无需担心一个慢速系统调用是否被中断:
+不管系统是否重新启动中断的系统调用，该程序会如所预期的工作，
+但仍存在于其他信号处理程序交互的问题
 */
