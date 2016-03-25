711311498adaa5cbd608ecc1451273a98a04f226
leo <leo07046219@gmail.com>
2016-03-25 08:35:12 +0800

    10-10--lib--prmask.c

---
+++ lib/prmask.c
@@ -0,0 +1,45 @@
+/*10-10 为进程打印信号屏蔽字*/
+
+#include "apue.h"
+#include <errno.h>
+
+void pr_mask(const char *str)
+{
+	sigset_t	sigset;
+	int			errno_save = 0;
+
+	errno_save = errno;		/* we can be called by signal handlers */
+
+    if (sigprocmask(0, NULL, &sigset) < 0)
+    {
+        err_sys("sigprocmask error");
+    }
+
+	printf("%s", str);
+
+    if (sigismember(&sigset, SIGINT))
+    {
+        printf("SIGINT ");
+    }
+
+    if (sigismember(&sigset, SIGQUIT))
+    {
+        printf("SIGQUIT ");
+    }
+
+    if (sigismember(&sigset, SIGUSR1))
+    {
+        printf("SIGUSR1 ");
+    }
+
+    if (sigismember(&sigset, SIGALRM))
+    {
+        printf("SIGALRM ");
+    }
+
+	/* remaining signals can go here  */
+
+	printf("\n");
+
+	errno = errno_save;
+}
