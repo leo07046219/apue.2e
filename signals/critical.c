c504afefdf637cd499f1659ad1a45182799c676c
leo <leo07046219@gmail.com>
2016-03-26 09:45:35 +0800

    10-11--signals--critical.c添加註釋

--- signals/critical.c
+++ signals/critical.c
@@ -4,13 +4,14 @@
 
 static void	sig_quit(int);
 
-int
-main(void)
+int main(void)
 {
 	sigset_t	newmask, oldmask, pendmask;
 
-	if (signal(SIGQUIT, sig_quit) == SIG_ERR)
-		err_sys("can't catch SIGQUIT");
+    if (signal(SIGQUIT, sig_quit) == SIG_ERR)
+    {
+        err_sys("can't catch SIGQUIT");
+    }
 
 	/*
 	 * Block SIGQUIT and save current signal mask.
@@ -18,33 +19,46 @@ main(void)
 	sigemptyset(&newmask);
 	sigaddset(&newmask, SIGQUIT);
 
-	if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
-		err_sys("SIG_BLOCK error");
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
+    {
+        err_sys("SIG_BLOCK error");
+    }
 
 	sleep(5);	/* SIGQUIT here will remain pending */
 
-	if (sigpending(&pendmask) < 0)
-		err_sys("sigpending error");
-	if (sigismember(&pendmask, SIGQUIT))
-		printf("\nSIGQUIT pending\n");
+    if (sigpending(&pendmask) < 0)
+    {
+        err_sys("sigpending error");
+    }
+
+    if (sigismember(&pendmask, SIGQUIT))
+    {
+        printf("\nSIGQUIT pending\n");
+    }
 
 	/*
 	 * Reset signal mask which unblocks SIGQUIT.
 	 */
-	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
-		err_sys("SIG_SETMASK error");
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+    {
+        err_sys("SIG_SETMASK error");
+    }
 	printf("SIGQUIT unblocked\n");
 
 	sleep(5);	/* SIGQUIT here will terminate with core file */
+
 	exit(0);
 }
 
-static void
-sig_quit(int signo)
+static void sig_quit(int signo)
 {
 	printf("caught SIGQUIT\n");
-	if (signal(SIGQUIT, SIG_DFL) == SIG_ERR)
-		err_sys("can't reset SIGQUIT");
+
+    if (signal(SIGQUIT, SIG_DFL) == SIG_ERR)
+    {
+        err_sys("can't reset SIGQUIT");
+    }
 }
 /*
+信号被阻塞时，同种信号发生多次，解除阻塞后只响应一次
 */
