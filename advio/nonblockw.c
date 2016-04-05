7b49ef2bc48aabf63f8e23209190c1a6fccac041
leo <leo07046219@gmail.com>
2016-04-05 08:26:56 +0800

    14-1--advio--nonblockw.c添加注释

--- advio/nonblockw.c
+++ advio/nonblockw.c
@@ -4,32 +4,37 @@
 #include <errno.h>
 #include <fcntl.h>
 
-char	buf[500000];
+#define MAX_BUF_LEN     500000
 
-int
-main(void)
+char	buf[MAX_BUF_LEN];
+
+int main(void)
 {
 	int		ntowrite, nwrite;
-	char	*ptr;
+	char	*ptr = NULL;
 
 	ntowrite = read(STDIN_FILENO, buf, sizeof(buf));
 	fprintf(stderr, "read %d bytes\n", ntowrite);
 
-	set_fl(STDOUT_FILENO, O_NONBLOCK);	/* set nonblocking */
+    /*设置标准输出为非阻塞*/
+	set_fl(STDOUT_FILENO, O_NONBLOCK);	    /* set nonblocking */
 
 	ptr = buf;
-	while (ntowrite > 0) {
+	while (ntowrite > 0) 
+    {
 		errno = 0;
 		nwrite = write(STDOUT_FILENO, ptr, ntowrite);
 		fprintf(stderr, "nwrite = %d, errno = %d\n", nwrite, errno);
 
-		if (nwrite > 0) {
+		if (nwrite > 0) 
+        {
 			ptr += nwrite;
 			ntowrite -= nwrite;
 		}
 	}
 
-	clr_fl(STDOUT_FILENO, O_NONBLOCK);	/* clear nonblocking */
+	clr_fl(STDOUT_FILENO, O_NONBLOCK);	    /* clear nonblocking */
 
 	exit(0);
 }
+/*从标准输入读，写到标准输出上*/
