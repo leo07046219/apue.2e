3bc419d0aabe18883ab704d250d477b1719aff17
leo <leo07046219@gmail.com>
2016-04-12 08:18:06 +0800

    14-10--advio--catgetmsg.c添加注释

--- advio/catgetmsg.c
+++ advio/catgetmsg.c
@@ -5,27 +5,46 @@
 
 #define	BUFFSIZE	4096
 
-int
-main(void)
+int main(void)
 {
 	int				n, flag;
 	char			ctlbuf[BUFFSIZE], datbuf[BUFFSIZE];
 	struct strbuf	ctl, dat;
 
+    memset(ctlbuf, 0, sizeof(ctlbuf));
+    memset(datbuf, 0, sizeof(datbuf));
+
 	ctl.buf = ctlbuf;
 	ctl.maxlen = BUFFSIZE;
 	dat.buf = datbuf;
 	dat.maxlen = BUFFSIZE;
-	for ( ; ; ) {
-		flag = 0;		/* return any message */
-		if ((n = getmsg(STDIN_FILENO, &ctl, &dat, &flag)) < 0)
-			err_sys("getmsg error");
-		fprintf(stderr, "flag = %d, ctl.len = %d, dat.len = %d\n",
+
+	for ( ; ; ) 
+    {
+        /* return any message */
+		flag = 0;		
+        if ((n = getmsg(STDIN_FILENO, &ctl, &dat, &flag)) < 0)
+        {
+            err_sys("getmsg error");
+        }
+
+		fprintf(stderr, "flag = %d, ctl.len = %d, dat.len = %d\n", \
 		  flag, ctl.len, dat.len);
-		if (dat.len == 0)
-			exit(0);
-		else if (dat.len > 0)
-			if (write(STDOUT_FILENO, dat.buf, dat.len) != dat.len)
-				err_sys("write error");
+
+        if (0 == dat.len)
+        {
+            exit(0);
+        }
+        else if (dat.len > 0)
+        {
+            if (write(STDOUT_FILENO, dat.buf, dat.len) != dat.len)
+            {
+                err_sys("write error");
+            }
+        }
 	}
 }
+/*
+apue.2e/advio$ echo hello, world! | ./catgetmsg
+getmsg error: Function not implemented
+*/
