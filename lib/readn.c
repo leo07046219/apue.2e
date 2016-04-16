6cb5e69b6b95fb7fb0f47c16b79a3c9bbdd3c9ed
leo <leo07046219@gmail.com>
2016-04-16 09:55:40 +0800

    14-11--lib--readn.c

--- lib/readn.c
+++ lib/readn.c
@@ -19,16 +19,16 @@ readn(int fd, void *ptr, size_t n)
         {
             if (nleft == n)
             {
-                return(-1); /* error, return -1 */
+                return(-1); /* error, return -1 一个字节都没读到，返回出错*/
             }
             else
             {
-                break;      /* error, return amount read so far */
+                break;      /* error, return amount read so far 都到一些字节，返回已读数目*/
             }
 		} 
         else if (0 == nread) 
         {
-			break;          /* EOF */
+			break;          /* EOF 读完*/
 		}
 
 		nleft -= nread;
@@ -38,5 +38,5 @@ readn(int fd, void *ptr, size_t n)
 	return(n - nleft);      /* return >= 0 */
 }
 /*
-
+1.对于管道、FIFO以及某些终端、网络和STREAM设备，一次read/write的字节数可能小于指定值，谢需要多次调用read/write直至达到指定值；
 */
