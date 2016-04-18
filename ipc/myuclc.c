9fc39ef7d9be662e220f2bbbc28b80d26911ab60
leo <leo07046219@gmail.com>
2016-04-18 08:30:02 +0800

    15-5--ipc--myuclc.c

---
+++ ipc/myuclc.c
@@ -0,0 +1,30 @@
+/*15-6 将大写字符换成小写字符的过滤程序*/
+
+#include "apue.h"
+#include <ctype.h>
+
+int main(void)
+{
+	int		c = 0;
+
+	while ((c = getchar()) != EOF) 
+    {
+        if (isupper(c))
+        {
+            c = tolower(c);
+        }
+        if (putchar(c) == EOF)
+        {
+            err_sys("output error");
+        }
+        if (c == '\n')
+        {
+            fflush(stdout);
+        }
+	}
+
+	exit(0);
+}
+/*
+将标准输入复制到标准输出，并大写转小写，行缓冲；
+*/
