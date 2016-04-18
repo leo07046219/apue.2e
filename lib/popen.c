8ec9ee754c95152dfcab0fa807118f19ce1934ae
leo <leo07046219@gmail.com>
2016-04-18 08:21:29 +0800

    15-5--lib--popen.c添加注释

--- lib/popen.c
+++ lib/popen.c
@@ -15,95 +15,134 @@ static pid_t	*childpid = NULL;
  */
 static int		maxfd;
 
-FILE *
-popen(const char *cmdstring, const char *type)
+FILE * popen(const char *cmdstring, const char *type)
 {
-	int		i;
+	int		i = 0;
 	int		pfd[2];
 	pid_t	pid;
-	FILE	*fp;
+	FILE	*fp = NULL;
+
+    memset(pfd, 0, sizeof(pfd));
 
 	/* only allow "r" or "w" */
-	if ((type[0] != 'r' && type[0] != 'w') || type[1] != 0) {
-		errno = EINVAL;		/* required by POSIX */
+	if ((type[0] != 'r' && type[0] != 'w') || type[1] != 0) 
+    {
+		errno = EINVAL;     /* required by POSIX */
 		return(NULL);
 	}
 
-	if (childpid == NULL) {		/* first time through */
+	if (NULL == childpid) 
+    {		
+        /* first time through */
 		/* allocate zeroed out array for child pids */
 		maxfd = open_max();
-		if ((childpid = calloc(maxfd, sizeof(pid_t))) == NULL)
-			return(NULL);
+        /*一个进程可能调用多次popen，pid数组大小为maxfd*/
+        if ((childpid = calloc(maxfd, sizeof(pid_t))) == NULL)
+        {
+            return(NULL);
+        }
 	}
 
-	if (pipe(pfd) < 0)
-		return(NULL);	/* errno set by pipe() */
+    if (pipe(pfd) < 0)
+    {
+        return(NULL);       /* errno set by pipe() */
+    }
 
-	if ((pid = fork()) < 0) {
-		return(NULL);	/* errno set by fork() */
-	} else if (pid == 0) {							/* child */
-		if (*type == 'r') {
+	if ((pid = fork()) < 0) 
+    {
+		return(NULL);       /* errno set by fork() */
+	} 
+    else if (0 == pid) 
+    {
+        /* child */
+		if ('r' == *type) 
+        {
 			close(pfd[0]);
-			if (pfd[1] != STDOUT_FILENO) {
+			if (pfd[1] != STDOUT_FILENO) 
+            {
 				dup2(pfd[1], STDOUT_FILENO);
 				close(pfd[1]);
 			}
-		} else {
+		} 
+        else 
+        {
 			close(pfd[1]);
-			if (pfd[0] != STDIN_FILENO) {
+			if (pfd[0] != STDIN_FILENO) 
+            {
 				dup2(pfd[0], STDIN_FILENO);
 				close(pfd[0]);
 			}
 		}
 
 		/* close all descriptors in childpid[] */
-		for (i = 0; i < maxfd; i++)
-			if (childpid[i] > 0)
-				close(i);
+        for (i = 0; i < maxfd; i++)
+        {
+            if (childpid[i] > 0)
+            {
+                close(i);
+            }
+        }
 
 		execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);
 		_exit(127);
 	}
 
 	/* parent continues... */
-	if (*type == 'r') {
+	if ('r' == *type) 
+    {
 		close(pfd[1]);
-		if ((fp = fdopen(pfd[0], type)) == NULL)
-			return(NULL);
-	} else {
+        if ((fp = fdopen(pfd[0], type)) == NULL)
+        {
+            return(NULL);
+        }
+	} 
+    else 
+    {
 		close(pfd[0]);
-		if ((fp = fdopen(pfd[1], type)) == NULL)
-			return(NULL);
+        if ((fp = fdopen(pfd[1], type)) == NULL)
+        {
+            return(NULL);
+        }
 	}
 
 	childpid[fileno(fp)] = pid;	/* remember child pid for this fd */
 	return(fp);
 }
 
-int
-pclose(FILE *fp)
+int pclose(FILE *fp)
 {
 	int		fd, stat;
 	pid_t	pid;
 
-	if (childpid == NULL) {
+	if (NULL == childpid) 
+    {
 		errno = EINVAL;
 		return(-1);		/* popen() has never been called */
 	}
 
 	fd = fileno(fp);
-	if ((pid = childpid[fd]) == 0) {
+	if ((pid = childpid[fd]) == 0) 
+    {
 		errno = EINVAL;
-		return(-1);		/* fp wasn't opened by popen() */
+		return(-1);     /* fp wasn't opened by popen() */
 	}
 
 	childpid[fd] = 0;
-	if (fclose(fp) == EOF)
-		return(-1);
+    if (fclose(fp) == EOF)
+    {
+        return(-1);
+    }
 
-	while (waitpid(pid, &stat, 0) < 0)
-		if (errno != EINTR)
-			return(-1);	/* error other than EINTR from waitpid() */
+    while (waitpid(pid, &stat, 0) < 0)
+    {
+        if (errno != EINTR)
+        {
+            return(-1); /* error other than EINTR from waitpid() */
+        }
+    }
 
-	return(stat);	/* return child's termination status */
+	return(stat);       /* return child's termination status */
 }
+/*
+1.popen决不应由设置用户ID或设置组ID程序调用，可以非法提升权限
+*/
