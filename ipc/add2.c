6856bd1a9948ad7aaccfcbfbdabf31469e673b69
leo <leo07046219@gmail.com>
2016-04-19 08:12:51 +0800

    15-8--ipc--add2.c添加注释

--- ipc/add2.c
+++ ipc/add2.c
@@ -4,23 +4,36 @@
 
 int main(void)
 {
-	int		n, int1, int2;
+	int		n = 0, int1 = 0, int2 = 0;
 	char	line[MAXLINE];
 
-	while ((n = read(STDIN_FILENO, line, MAXLINE)) > 0) {
+    memset(line, 0, sizeof(line));
+
+	while ((n = read(STDIN_FILENO, line, MAXLINE)) > 0) 
+    {
 		line[n] = 0;		/* null terminate */
-		if (sscanf(line, "%d%d", &int1, &int2) == 2) {
+
+		if (sscanf(line, "%d%d", &int1, &int2) == 2) 
+        {
 			sprintf(line, "%d\n", int1 + int2);
 			n = strlen(line);
-			if (write(STDOUT_FILENO, line, n) != n)
-				err_sys("write error");
-		} else {
-			if (write(STDOUT_FILENO, "invalid args\n", 13) != 13)
-				err_sys("write error");
+
+            if (write(STDOUT_FILENO, line, n) != n)
+            {
+                err_sys("write error");
+            }
+		} 
+        else 
+        {
+            if (write(STDOUT_FILENO, "invalid args\n", 13) != 13)
+            {
+                err_sys("write error");
+            }
 		}
 	}
+
 	exit(0);
 }
 /*
-
+从标准输入读2个数，求和，写到标准输出
 */
