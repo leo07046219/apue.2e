874e3b1e5ec8e8c18b9120ef4e28e0c14cf37173
leo <leo07046219@gmail.com>
2016-04-25 08:55:10 +0800

    16-4--socket--ruptime.c

--- sockets/ruptime.c
+++ sockets/ruptime.c
@@ -10,29 +10,40 @@
 
 extern int connect_retry(int, const struct sockaddr *, socklen_t);
 
+/*读取并显示服务器发送的数据*/
 void print_uptime(int sockfd)
 {
-	int		n;
+	int		n = 0;
 	char	buf[BUFLEN];
 
-	while ((n = recv(sockfd, buf, BUFLEN, 0)) > 0)
-		write(STDOUT_FILENO, buf, n);
-	if (n < 0)
-		err_sys("recv error");
+    memset(buf, 0, sizeof(buf));
+
+    /*面向连接的，循环读取直到返回0*/
+    while ((n = recv(sockfd, buf, BUFLEN, 0)) > 0)
+    {
+        write(STDOUT_FILENO, buf, n);
+    }
+
+    if (n < 0)
+    {
+        err_sys("recv error");
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	struct  addrinfo	*ailist, *aip;
+	struct  addrinfo	*pAilist = NULL, *pAip = NULL;
 	struct  addrinfo	hint;
-	int     sockfd, err;
+	int     sockfd = 0, err = 0;
 
     if (argc != 2)
     {
         err_quit("usage: ruptime hostname");
     }
 
-	hint.ai_flags = 0;
+    memset((char *)&hint, 0, sizeof(hint));
+
+	hint.ai_flags = AI_PASSIVE;
 	hint.ai_family = 0;
 	hint.ai_socktype = SOCK_STREAM;
 	hint.ai_protocol = 0;
@@ -41,19 +52,20 @@ int main(int argc, char *argv[])
 	hint.ai_addr = NULL;
 	hint.ai_next = NULL;
 
-    if ((err = getaddrinfo(argv[1], "ruptime", &hint, &ailist)) != 0)
+    /*如果服务器支持多重网络接口或多重网络协议，getaddrinfo()会返回不止一个候选地址*/
+    if ((err = getaddrinfo(argv[1], "4000", &hint, &pAilist)) != 0)
     {
         err_quit("getaddrinfo error: %s", gai_strerror(err));
     }
-
-	for (aip = ailist; aip != NULL; aip = aip->ai_next) 
+    /*轮流尝试每个地址，直到找到一个允许连接服务的地址*/
+	for (pAip = pAilist; pAip != NULL; pAip = pAip->ai_next) 
     {
-        if ((sockfd = socket(aip->ai_family, SOCK_STREAM, 0)) < 0)
+        if ((sockfd = socket(pAip->ai_family, SOCK_STREAM, 0)) < 0)
         {
             err = errno;
         }
-
-		if (connect_retry(sockfd, aip->ai_addr, aip->ai_addrlen) < 0) 
+        /*面向连接的协议，需要在交换数据前连接服务器*/
+		if (connect_retry(sockfd, pAip->ai_addr, pAip->ai_addrlen) < 0) 
         {
 			err = errno;
 		} 
@@ -68,3 +80,11 @@ int main(int argc, char *argv[])
 
 	exit(1);
 }
+/*
+1.与服务器通信以获得系统命令uptime的输出；
+2.入口参数是hostname  ？
+以端口号的方式连接服务器：这个就有一点需要注意，hint的ai_flags不要设为AI_PASSIVE，因为返回的地址不是用来监听绑定的，一般设为AI_CANONNAME
+apue.2e/sockets$ ./ruptime ubuntu
+08:51:29 up 4 days,  2:21,  4 users,  load average: 0.00, 0.01, 0.05
+
+*/
