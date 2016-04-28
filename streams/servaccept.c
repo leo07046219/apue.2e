ece3a1eed3cc5a6c55ed39565dd32b399fd938c3
leo <leo07046219@gmail.com>
2016-04-28 08:52:05 +0800

    17-4--streams--servaccept.c添加注释

--- streams/servaccept.c
+++ streams/servaccept.c
@@ -1,4 +1,4 @@
-/*17-4 使用STREAMS管道的serv_accept函数*/
+/*17-4 使用STREAMS管道的serv_accept函数，在侦听管道上等待客户端连接到服务器*/
 
 #include "apue.h"
 #include <stropts.h>
@@ -12,9 +12,19 @@ int serv_accept(int listenfd, uid_t *uidptr)
 {
 	struct strrecvfd	recvfd;
 
-	if (ioctl(listenfd, I_RECVFD, &recvfd) < 0)
-		return(-1);		            /* could be EINTR if signal caught */
-	if (uidptr != NULL)
-		*uidptr = recvfd.uid;       /* effective uid of caller */
+    memset((char *)&recvfd, 0, sizeof(recvfd));
+
+    /*当客户端连接请求到达时，系统自动创建一个新的STREAM管道，
+    返回的recvfd结构包含管道fd和用户id*/
+    if (ioctl(listenfd, I_RECVFD, &recvfd) < 0)
+    {
+        return(-1);		            /* could be EINTR if signal caught */
+    }
+
+    if (uidptr != NULL)
+    {
+        *uidptr = recvfd.uid;       /* effective uid of caller */
+    }
+
 	return(recvfd.fd);              /* return the new descriptor */
 }
