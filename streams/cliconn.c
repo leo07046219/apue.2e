ff65b5a5ff0fabe9583115f7b62c106e59b47e5b
leo <leo07046219@gmail.com>
2016-04-28 08:55:12 +0800

    17-5--streams--cliconn.c添加注释

--- streams/cliconn.c
+++ streams/cliconn.c
@@ -1,4 +1,4 @@
-/*17-5 用STREAMS管道的cli_conn函数连接服务器，获取文件描述符*/
+/*17-5 通过众所周知的名字，用STREAMS管道的cli_conn函数连接服务器，获取文件描述符*/
 
 #include "apue.h"
 #include <fcntl.h>
@@ -8,17 +8,21 @@
  * Create a client endpoint and connect to a server.
  * Returns fd if all OK, <0 on error.
  */
-int
-cli_conn(const char *name)
+int cli_conn(const char *name)
 {
-	int		fd;
+	int		fd = 0;
 
 	/* open the mounted stream */
-	if ((fd = open(name, O_RDWR)) < 0)
-		return(-1);
-	if (isastream(fd) == 0) {
+    if ((fd = open(name, O_RDWR)) < 0)
+    {
+        return(-1);
+    }
+    /*以防服务器进程没有启动，而路径名仍存在于文件系统中*/
+	if (isastream(fd) == 0) 
+    {
 		close(fd);
 		return(-2);
 	}
+
 	return(fd);
 }
