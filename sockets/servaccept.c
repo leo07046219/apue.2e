20d8c7795b588d998cc0e9636c5a2937da7f5459
leo <leo07046219@gmail.com>
2016-05-02 22:00:41 +0800

    17-9--sockets--servaccept.c添加注释

--- sockets/servaccept.c
+++ sockets/servaccept.c
@@ -16,51 +16,64 @@
  */
 int serv_accept(int listenfd, uid_t *uidptr)
 {
-	int					clifd, len, err, rval;
+	int					clifd = 0, len = 0, err = 0, rval = 0;
 	time_t				staletime;
 	struct sockaddr_un	un;
 	struct stat			statbuf;
 
 	len = sizeof(un);
-	if ((clifd = accept(listenfd, (struct sockaddr *)&un, &len)) < 0)
-		return(-1);		/* often errno=EINTR, if signal caught */
+    if ((clifd = accept(listenfd, (struct sockaddr *)&un, &len)) < 0)
+    {
+        return(-1);		            /* often errno=EINTR, if signal caught */
+    }
 
 	/* obtain the client's uid from its calling address */
 	len -= offsetof(struct sockaddr_un, sun_path); /* len of pathname */
-	un.sun_path[len] = 0;			/* null terminate */
+	un.sun_path[len] = 0;           /* null terminate */
 
-	if (stat(un.sun_path, &statbuf) < 0) {
+	if (stat(un.sun_path, &statbuf) < 0) 
+    {
 		rval = -2;
 		goto errout;
 	}
-#ifdef	S_ISSOCK	/* not defined for SVR4 */
-	if (S_ISSOCK(statbuf.st_mode) == 0) {
-		rval = -3;		/* not a socket */
+
+#ifdef	S_ISSOCK                    /* not defined for SVR4 */
+	if (S_ISSOCK(statbuf.st_mode) == 0) 
+    {
+		rval = -3;                  /* not a socket */
 		goto errout;
 	}
 #endif
+
 	if ((statbuf.st_mode & (S_IRWXG | S_IRWXO)) ||
-		(statbuf.st_mode & S_IRWXU) != S_IRWXU) {
-		  rval = -4;	/* is not rwx------ */
+		(statbuf.st_mode & S_IRWXU) != S_IRWXU) 
+    {
+		  rval = -4;                /* is not rwx------ */
 		  goto errout;
 	}
 
 	staletime = time(NULL) - STALE;
+
 	if (statbuf.st_atime < staletime ||
 		statbuf.st_ctime < staletime ||
-		statbuf.st_mtime < staletime) {
-		  rval = -5;	/* i-node is too old */
+		statbuf.st_mtime < staletime) 
+    {
+		  rval = -5;                /* i-node is too old */
 		  goto errout;
 	}
 
-	if (uidptr != NULL)
-		*uidptr = statbuf.st_uid;	/* return uid of caller */
-	unlink(un.sun_path);		/* we're done with pathname now */
+    if (uidptr != NULL)
+    {
+        *uidptr = statbuf.st_uid;   /* return uid of caller */
+    }
+
+	unlink(un.sun_path);            /* we're done with pathname now */
 	return(clifd);
 
 errout:
 	err = errno;
 	close(clifd);
 	errno = err;
+
 	return(rval);
 }
