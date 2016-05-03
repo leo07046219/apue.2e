5986d9efd9662806a57b32a30d934f3f221c3e01
leo <leo07046219@gmail.com>
2016-05-03 08:24:54 +0800

    17-13--streams-recvfd.c添加注释

--- streams/recvfd.c
+++ streams/recvfd.c
@@ -18,16 +18,25 @@ int recv_fd(int fd, ssize_t (*userfunc)(int, const void *, size_t))
 	struct strrecvfd	recvfd;
 
 	status = -1;
+    memset((char *)buf, 0, sizeof(buf));
+    memset((char *)&dat, 0, sizeof(dat));
+    memset((char *)&recvfd, 0, sizeof(recvfd));
 
 	for ( ; ; ) 
     {
 		dat.buf = buf;
 		dat.maxlen = MAXLINE;
 		flag = 0;
-		if (getmsg(fd, NULL, &dat, &flag) < 0)
-			err_sys("getmsg error");
+
+        if (getmsg(fd, NULL, &dat, &flag) < 0)
+        {
+            err_sys("getmsg error");
+        }
+
 		nread = dat.len;
-		if (nread == 0) {
+
+		if (0 == nread) 
+        {
 			err_ret("connection closed by server");
 			return(-1);
 		}
@@ -37,27 +46,47 @@ int recv_fd(int fd, ssize_t (*userfunc)(int, const void *, size_t))
 		 * Null must be next to last byte of buffer, status
 		 * byte is last byte.  Zero status means there must
 		 * be a file descriptor to receive.
+         接收双字节协议及fd
 		 */
-		for (ptr = buf; ptr < &buf[nread]; ) {
-			if (*ptr++ == 0) {
-				if (ptr != &buf[nread-1])
-					err_dump("message format error");
- 				status = *ptr & 0xFF;	/* prevent sign extension */
- 				if (status == 0) {
-					if (ioctl(fd, I_RECVFD, &recvfd) < 0)
-						return(-1);
+		for (ptr = buf; ptr < &buf[nread]; ) 
+        {
+			if (0 == *ptr++) 
+            {
+                if (ptr != &buf[nread - 1])
+                {
+                    err_dump("message format error");
+                }
+
+                status = *ptr & 0xFF;   /* prevent sign extension */
+
+                if (0 == status) 
+                {
+                    if (ioctl(fd, I_RECVFD, &recvfd) < 0)
+                    {
+                        return(-1);
+                    }
 					newfd = recvfd.fd;	/* new descriptor */
-				} else {
+				} 
+                else 
+                {
 					newfd = -status;
 				}
+
 				nread -= 2;
 			}
 		}
-		if (nread > 0)
-			if ((*userfunc)(STDERR_FILENO, buf, nread) != nread)
-				return(-1);
+        /*通过用户指定的接收函数接收后续数据*/
+        if (nread > 0)
+        {
+            if ((*userfunc)(STDERR_FILENO, buf, nread) != nread)
+            {
+                return(-1);
+            }
+        }
 
-		if (status >= 0)	/* final data has arrived */
-			return(newfd);	/* descriptor, or -status */
+        if (status >= 0)	/* final data has arrived */
+        {
+            return(newfd);	/* descriptor, or -status */
+        }
 	}
 }
