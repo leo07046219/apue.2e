3d39206e2c3945c54deae54b13e69a51e5643b0c
leo <leo07046219@gmail.com>
2016-05-09 08:04:34 +0800

    17-29--opend--main.c

---
+++ opend/main.c
@@ -0,0 +1,37 @@
+/*17-29 服务器进程main函数版本2*/
+
+#include	"opend.h"
+#include	<syslog.h>
+
+int     debug, oflag, client_size, log_to_stderr;
+char    errmsg[MAXLINE];
+char    *pathname;
+Client  *client = NULL;
+
+int main(int argc, char *argv[])
+{
+	int c = 0;
+
+	log_open("open.serv", LOG_PID, LOG_USER);
+
+	opterr = 0;         /* don't want getopt() writing to stderr */
+	while ((c = getopt(argc, argv, "d")) != EOF) 
+    {
+		switch (c) 
+        {
+		case 'd':       /* debug -d选项调用服务器进程，以交互方式运行*/
+			debug = log_to_stderr = 1;
+			break;
+
+		case '?':
+			err_quit("unrecognized option: -%c", optopt);
+		}
+	}
+
+    if (0 == debug)
+    {
+        daemonize("opend");
+    }
+
+	loop();             /* never returns */
+}
