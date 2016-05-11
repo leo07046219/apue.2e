6f7c08a08505ba70b6f25b87faebba23afa5b770
leo <leo07046219@gmail.com>
2016-05-11 08:15:43 +0800

    18-5--termios--t_isatty.c

---
+++ termios/t_isatty.c
@@ -0,0 +1,12 @@
+/*18-5 ²âÊÔisattyº¯Êı*/
+
+#include "apue.h"
+
+int main(void)
+{
+	printf("fd 0: %s\n", isatty(0) ? "tty" : "not a tty");
+	printf("fd 1: %s\n", isatty(1) ? "tty" : "not a tty");
+	printf("fd 2: %s\n", isatty(2) ? "tty" : "not a tty");
+
+	exit(0);
+}
