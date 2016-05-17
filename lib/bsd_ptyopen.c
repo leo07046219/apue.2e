30343456502d3f6e23e7dbc7f0fb996548767668
leo <leo07046219@gmail.com>
2016-05-17 08:20:24 +0800

    19-2--lib--bsd_ptyopen.c

--- lib/bsd_ptyopen.c
+++ lib/bsd_ptyopen.c
@@ -6,29 +6,38 @@
 #include <grp.h>
 
 #ifndef _HAS_OPENPT
-int
-posix_openpt(int oflag)
+int posix_openpt(int oflag)
 {
-	int		fdm;
-	char	*ptr1, *ptr2;
+	int		fdm = 0;
+	char	*ptr1 = NULL, *ptr2 = NULL;
 	char	ptm_name[16];
 
+    /*遍历打开所有可能的pty主设备名，直到成功打开一个可用的，或试完所有的*/
 	strcpy(ptm_name, "/dev/ptyXY");
 	/* array index:   0123456789   (for references in following code) */
-	for (ptr1 = "pqrstuvwxyzPQRST"; *ptr1 != 0; ptr1++) {
+	for (ptr1 = "pqrstuvwxyzPQRST"; *ptr1 != 0; ptr1++) 
+    {
 		ptm_name[8] = *ptr1;
-		for (ptr2 = "0123456789abcdef"; *ptr2 != 0; ptr2++) {
+		for (ptr2 = "0123456789abcdef"; *ptr2 != 0; ptr2++) 
+        {
 			ptm_name[9] = *ptr2;
 
 			/*
 			 * Try to open the master.
 			 */
-			if ((fdm = open(ptm_name, oflag)) < 0) {
-				if (errno == ENOENT)	/* different from EIO */
-					return(-1);			/* out of pty devices */
-				else
-					continue;			/* try next pty device */
+			if ((fdm = open(ptm_name, oflag)) < 0) 
+            {
+                /*设备不存在*/
+                if (errno == ENOENT)	/* different from EIO */
+                {
+                    return(-1);			/* out of pty devices */
+                }
+                else
+                {
+                    continue;			/* try next pty device */
+                }
 			}
+
 			return(fdm);		/* got it, return fd of master */
 		}
 	}
@@ -38,61 +47,75 @@ posix_openpt(int oflag)
 #endif
 
 #ifndef _HAS_PTSNAME
-char *
-ptsname(int fdm)
+/*生成pty从设备名*/
+char *ptsname(int fdm)
 {
 	static char	pts_name[16];
-	char		*ptm_name;
+	char		*ptm_name = NULL;
+
+    memset(pts_name, 0, sizeof(pts_name));
 
 	ptm_name = ttyname(fdm);
-	if (ptm_name == NULL)
-		return(NULL);
+    if (NULL == ptm_name)
+    {
+        return(NULL);
+    }
 	strncpy(pts_name, ptm_name, sizeof(pts_name));
 	pts_name[sizeof(pts_name) - 1] = '\0';
-	if (strncmp(pts_name, "/dev/pty/", 9) == 0)
-		pts_name[9] = 's';	/* change /dev/pty/mXX to /dev/pty/sXX */
-	else
-		pts_name[5] = 't';	/* change "pty" to "tty" */
+
+    if (strncmp(pts_name, "/dev/pty/", 9) == 0)
+    {
+        pts_name[9] = 's';	/* change /dev/pty/mXX to /dev/pty/sXX */
+    }
+    else
+    {
+        pts_name[5] = 't';	/* change "pty" to "tty" */
+    }
 	return(pts_name);
 }
 #endif
 
 #ifndef _HAS_GRANTPT
-int
-grantpt(int fdm)
+int grantpt(int fdm)
 {
-	struct group	*grptr;
-	int				gid;
-	char			*pts_name;
+	struct group	*grptr = NULL;
+	int				gid = 0;
+	char			*pts_name = NULL;
 
 	pts_name = ptsname(fdm);
-	if ((grptr = getgrnam("tty")) != NULL)
-		gid = grptr->gr_gid;
-	else
-		gid = -1;		/* group tty is not in the group file */
+    if ((grptr = getgrnam("tty")) != NULL)
+    {
+        gid = grptr->gr_gid;
+    }
+    else
+    {
+        gid = -1;		/* group tty is not in the group file */
+    }
 
 	/*
 	 * The following two calls won't work unless we're the superuser.
 	 */
-	if (chown(pts_name, getuid(), gid) < 0)
-		return(-1);
+    if (chown(pts_name, getuid(), gid) < 0)
+    {
+        return(-1);
+    }
+
 	return(chmod(pts_name, S_IRUSR | S_IWUSR | S_IWGRP));
 }
 #endif
 
 #ifndef _HAS_UNLOCKPT
-int
-unlockpt(int fdm)
+int unlockpt(int fdm)
 {
-	return(0); /* nothing to do */
+    /* nothing to do */
+	return(0); 
 }
 #endif
 
-int
-ptym_open(char *pts_name, int pts_namesz)
+int ptym_open(char *pts_name, int pts_namesz)
 {
-	char	*ptr;
-	int		fdm;
+	char	*ptr = NULL;
+	int		fdm = 0;
 
 	/*
 	 * Return the name of the master device so that on failure
@@ -101,17 +124,29 @@ ptym_open(char *pts_name, int pts_namesz)
 	 */
 	strncpy(pts_name, "/dev/ptyXX", pts_namesz);
 	pts_name[pts_namesz - 1] = '\0';
-	if ((fdm = posix_openpt(O_RDWR)) < 0)
-		return(-1);
-	if (grantpt(fdm) < 0) {		/* grant access to slave */
+
+    if ((fdm = posix_openpt(O_RDWR)) < 0)
+    {
+        return(-1);
+    }
+
+    /* grant access to slave */
+	if (grantpt(fdm) < 0) 
+    {		
 		close(fdm);
 		return(-2);
 	}
-	if (unlockpt(fdm) < 0) {	/* clear slave's lock flag */
+
+    /* clear slave's lock flag */
+	if (unlockpt(fdm) < 0) 
+    {	
 		close(fdm);
 		return(-3);
 	}
-	if ((ptr = ptsname(fdm)) == NULL) {	/* get slave's name */
+
+    /* get slave's name */
+	if (NULL == (ptr = ptsname(fdm))) 
+    {	
 		close(fdm);
 		return(-4);
 	}
@@ -122,15 +157,19 @@ ptym_open(char *pts_name, int pts_namesz)
 	 */
 	strncpy(pts_name, ptr, pts_namesz);
 	pts_name[pts_namesz - 1] = '\0';
-	return(fdm);			/* return fd of master */
+
+    /* return fd of master */
+	return(fdm);			
 }
 
-int
-ptys_open(char *pts_name)
+int ptys_open(char *pts_name)
 {
-	int fds;
+	int fds = 0;
 
-	if ((fds = open(pts_name, O_RDWR)) < 0)
-		return(-5);
+    if ((fds = open(pts_name, O_RDWR)) < 0)
+    {
+        return(-5);
+    }
+
 	return(fds);
 }
