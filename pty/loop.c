d3ac0f21d153c2b25658ac3195c28064461ce7bc
leo <leo07046219@gmail.com>
2016-05-19 07:54:11 +0800

    19-6--pty--loop.c

--- pty/loop.c
+++ pty/loop.c
@@ -7,45 +7,67 @@
 static void	sig_term(int);
 static volatile sig_atomic_t	sigcaught;	/* set by signal handler */
 
-void
-loop(int ptym, int ignoreeof)
+void loop(int ptym, int ignoreeof)
 {
 	pid_t	child;
-	int		nread;
+	int		nread = 0;
 	char	buf[BUFFSIZE];
 
-	if ((child = fork()) < 0) {
+    memset(buf, 0, sizeof(buf));
+
+	if ((child = fork()) < 0) 
+    {
 		err_sys("fork error");
-	} else if (child == 0) {	/* child copies stdin to ptym */
-		for ( ; ; ) {
-			if ((nread = read(STDIN_FILENO, buf, BUFFSIZE)) < 0)
-				err_sys("read error from stdin");
-			else if (nread == 0)
-				break;		/* EOF on stdin means we're done */
-			if (writen(ptym, buf, nread) != nread)
-				err_sys("writen error to master pty");
+	} 
+    else if (0 == child) 
+    {	
+        /* child copies stdin to ptym */
+		for ( ; ; ) 
+        {
+            if ((nread = read(STDIN_FILENO, buf, BUFFSIZE)) < 0)
+            {
+                err_sys("read error from stdin");
+            }
+            else if (0 == nread)
+            {
+                break;		/* EOF on stdin means we're done */
+            }
+            if (writen(ptym, buf, nread) != nread)
+            {
+                err_sys("writen error to master pty");
+            }
 		}
 
 		/*
 		 * We always terminate when we encounter an EOF on stdin,
 		 * but we notify the parent only if ignoreeof is 0.
 		 */
-		if (ignoreeof == 0)
-			kill(getppid(), SIGTERM);	/* notify parent */
+        if (0 == ignoreeof)
+        {
+            kill(getppid(), SIGTERM);	/* notify parent */
+        }
+
 		exit(0);	/* and terminate; child can't return */
 	}
 
 	/*
 	 * Parent copies ptym to stdout.
 	 */
-	if (signal_intr(SIGTERM, sig_term) == SIG_ERR)
-		err_sys("signal_intr error for SIGTERM");
+    if (signal_intr(SIGTERM, sig_term) == SIG_ERR)
+    {
+        err_sys("signal_intr error for SIGTERM");
+    }
 
-	for ( ; ; ) {
-		if ((nread = read(ptym, buf, BUFFSIZE)) <= 0)
-			break;		/* signal caught, error, or EOF */
-		if (writen(STDOUT_FILENO, buf, nread) != nread)
-			err_sys("writen error to stdout");
+	for ( ; ; ) 
+    {
+        if ((nread = read(ptym, buf, BUFFSIZE)) <= 0)
+        {
+            break;		/* signal caught, error, or EOF */
+        }
+        if (writen(STDOUT_FILENO, buf, nread) != nread)
+        {
+            err_sys("writen error to stdout");
+        }
 	}
 
 	/*
@@ -53,8 +75,10 @@ loop(int ptym, int ignoreeof)
 	 * SIGTERM from the child, we read an EOF on the pty master (which
 	 * means we have to signal the child to stop), or an error.
 	 */
-	if (sigcaught == 0)	/* tell child if it didn't send us the signal */
-		kill(child, SIGTERM);
+    if (0 == sigcaught)	/* tell child if it didn't send us the signal */
+    {
+        kill(child, SIGTERM);
+    }
 	/*
 	 * Parent returns to caller.
 	 */
@@ -64,8 +88,10 @@ loop(int ptym, int ignoreeof)
  * The child sends us SIGTERM when it gets EOF on the pty slave or
  * when read() fails.  We probably interrupted the read() of ptym.
  */
-static void
-sig_term(int signo)
+static void sig_term(int signo)
 {
 	sigcaught = 1;		/* just set flag and return */
 }
+/*
+使用2个进程时，如果一个终止，那么它必须通知另一个，此处用SIGTERM信号实现
+*/
