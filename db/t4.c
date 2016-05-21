b9f04f737fee24708b81723d21f967622c22b9af
leo <leo07046219@gmail.com>
2016-05-21 09:02:02 +0800

    20-1--db--t4.c

--- db/t4.c
+++ db/t4.c
@@ -4,22 +4,44 @@
 #include "apue_db.h"
 #include <fcntl.h>
 
-int
-main(void)
+int main(void)
 {
-	DBHANDLE	db;
+	DBHANDLE	db = NULL;
 
-	if ((db = db_open("db4", O_RDWR | O_CREAT | O_TRUNC,
-	  FILE_MODE)) == NULL)
-		err_sys("db_open error");
+    if (NULL == (db = db_open("db4", O_RDWR | O_CREAT | O_TRUNC, \
+        FILE_MODE)))
+    {
+        err_sys("db_open error");
+    }
 
-	if (db_store(db, "Alpha", "data1", DB_INSERT) != 0)
-		err_quit("db_store error for alpha");
-	if (db_store(db, "beta", "Data for beta", DB_INSERT) != 0)
-		err_quit("db_store error for beta");
-	if (db_store(db, "gamma", "record3", DB_INSERT) != 0)
-		err_quit("db_store error for gamma");
+    if (db_store(db, "Alpha", "data1", DB_INSERT) != 0)
+    {
+        err_quit("db_store error for alpha");
+    }
+
+    if (db_store(db, "beta", "Data for beta", DB_INSERT) != 0)
+    {
+        err_quit("db_store error for beta");
+    }
+
+    if (db_store(db, "gamma", "record3", DB_INSERT) != 0)
+    {
+        err_quit("db_store error for gamma");
+    }
 
 	db_close(db);
+
 	exit(0);
 }
+/*
+.idx文件：
+0       53      35      0
+0       10Alpha:0:6
+0       10beta:6:14
+17      11gamma:20:8
+.dat文件：
+data1
+Data for beta
+reccord3
+
+*/
